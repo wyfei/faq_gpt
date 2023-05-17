134cb7c9438289a0de38fc0310791d1be03c2245
wyfei <wangyf@visvachina.com>
2023-05-17 23:48:44 +0800

    文档分割方式

--- gpt/qa_chain.py
+++ gpt/qa_chain.py
@@ -29,7 +29,7 @@ def get_docsearch():
     
     from langchain.text_splitter import CharacterTextSplitter
     text_splitter = CharacterTextSplitter(
-        separator = "<END>",
+        separator = "\n\n",
         chunk_size = 1000,
         chunk_overlap = 0,
         length_function = len,
