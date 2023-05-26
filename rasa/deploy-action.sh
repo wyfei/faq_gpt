d562e99980249637264f78471de605e5e7edf4af
wyfei <wangyf@visvachina.com>
2023-05-26 21:50:00 +0800

    docker file

--- rasa/deploy-action.sh
+++ rasa/deploy-action.sh
@@ -3,7 +3,7 @@
 today=` date "+%Y%m%d"`
 abbrev=`git log|head -1|cut -b 8-14`
 tag=$today-$abbrev
-docker build -t registry-vpc.cn-beijing.aliyuncs.com/visva/bi_rasa_action:$tag .
+docker build -t registry-vpc.cn-beijing.aliyuncs.com/visva/bi_rasa_action:$tag -f Dockerfile-action .
 if test `docker ps |grep bi_rasa_action|wc -l` == 1
  then
   echo 'container exists'
